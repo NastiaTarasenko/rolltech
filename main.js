6dfdedb12c8e8709352187fcc49beaae840c2d66
Nastia Tarasenko <anastasiatarasenko679@gmail.com>
2024-04-10 15:05:33 +0200

    slider

--- main.js
+++ main.js
@@ -16,3 +16,68 @@ MoreLessBtn.addEventListener("click", () => {
         btnText.innerHTML = "більше";
     }
 });
+
+// modal window
+
+const images = document.querySelectorAll(".gallery-image");
+const modalWindow = document.querySelector(".gallery-modal-wrapper");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+const modalOpen = () => {
+    modalWindow.style.display = "block";
+    document.body.style.overflow = "hidden";
+
+    slides.forEach(slide => {
+        slide.classList = ["slide"];
+    })
+    slides[currentSlide].classList.add("initial-slide");
+}
+
+images.forEach(image => {
+    image.addEventListener("click", () => {
+        currentSlide = [...images].indexOf(image);
+        modalOpen();
+    });
+});
+
+const prevSlide = () => {
+    slides.forEach(slide => {
+        slide.classList = ["slide"];
+    });
+    slides[currentSlide].classList.add("hide-to-right");
+    
+    currentSlide--;
+    if (currentSlide < 0) currentSlide = slides.length - 1;
+    
+    slides[currentSlide].classList.add("show-to-right");
+};
+const nextSlide = () => {
+    slides.forEach(slide => {
+        slide.classList = ["slide"];
+    });
+
+    slides[currentSlide].classList.add("hide-to-left");
+
+    currentSlide++;
+    if (currentSlide >= slides.length) currentSlide = 0;
+
+    slides[currentSlide].classList.add("show-to-left");
+};
+const changeSlideByTouch = (element) => {
+    element.addEventListener('touchstart', estart => {
+        startTouchX = estart.changedTouches[0].screenX;
+        element.addEventListener('touchend', eend => {
+            endTouchX = eend.changedTouches[0].screenX;
+            if (endTouchX > startTouchX) prevSlide();
+            else if (endTouchX < startTouchX) nextSlide();
+        });
+    });
+}
+
+slides.forEach(slide => changeSlideByTouch(slide));
+
+const modalClose = () => {
+    modalWindow.style.display = "none";
+    document.body.style.overflow = "auto";
+};
